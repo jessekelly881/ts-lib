1e6660e7312ec7fdfa3e30f8c05c4a848761bc47
Jesse Kelly <kellyjesse881@gmail.com>
2026-05-16 12:25:48 -0600

    .

--- src/git.ts
+++ src/git.ts
@@ -16,19 +16,20 @@ const defaultConfig: Required<GitBackingConfig> = {
 
 export type GitBacking = Layer.Layer<Backing, never, never>;
 
-export const gitBacking = (userConfig: GitBackingConfig): GitBacking => Layer.effect(Backing, Effect.gen(function*() {
+export const gitBacking = (userConfig: GitBackingConfig): GitBacking => Layer.scoped(Backing, Effect.gen(function*() {
     const config = { ...defaultConfig, ...userConfig }
 
     const git = simpleGit()
-    const currentBranch = yield* Effect.promise(() => git.revparse(["--abbrev-ref", "HEAD"]))
-    const tempBranchName = config.branchName(currentBranch)
-    yield* Effect.sync(() => git.checkoutBranch(tempBranchName, currentBranch))
+    const parentBranch = yield* Effect.promise(() => git.revparse(["--abbrev-ref", "HEAD"]))
+    const tempBranchName = config.branchName(parentBranch)
+    yield* Effect.sync(() => git.checkoutBranch(tempBranchName, parentBranch))
 
     const save = Effect.gen(function*() {
         const now = yield* Clock.currentTimeMillis
         yield* Effect.sync(() => git.add('./*').commit(now.toString()))
     })
 
+    yield* Effect.addFinalizer(() => Effect.sync(() => git.checkout(parentBranch)))
 
     return { save } 
 })) 
