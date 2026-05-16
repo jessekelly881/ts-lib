41a3d5ae4ed934e68c9d8aea7fb2b1026d8ad2e8
Jesse Kelly <kellyjesse881@gmail.com>
2026-05-16 12:39:26 -0600

    merged tdd code

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it } from "@effect/vitest"
+import { toRomanNumeral } from "@/x"
 
 describe("WorkflowEngine", () => {
 	it("works with TestClock", () => {
-		expect(1).toEqual(1)
+		expect(toRomanNumeral(1)).toEqual("I")
+		expect(toRomanNumeral(2)).toEqual("II")
+		expect(toRomanNumeral(5)).toEqual("V")
 	})
 }) 
